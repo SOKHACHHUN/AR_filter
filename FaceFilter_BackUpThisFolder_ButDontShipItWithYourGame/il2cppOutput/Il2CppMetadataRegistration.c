7a4d9ef06ad963e9076d2be79a35c96cdc4e9cf7
Roth <sokha3351@gmail.com>
2026-05-30 10:57:24 +0700

    Initial commit of Unity AR filter project

---
+++ FaceFilter_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	15868,
+	g_Il2CppGenericTypes,
+	11493,
+	g_Il2CppGenericInstTable,
+	143246,
+	g_Il2CppGenericMethodFunctions,
+	48499,
+	g_Il2CppTypeTable,
+	165861,
+	g_Il2CppMethodSpecTable,
+	14035,
+	g_FieldOffsetTable,
+	14035,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
